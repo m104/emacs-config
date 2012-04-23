533d71b9fd2fe3d416cf8ca8033f11fb266c39e0
Adam Collins <acollins@usbiotek.com>
2012-04-23 16:21:00 -0700

    wider frames not really needed at this point

--- system/gnu-linux.el
+++ system/gnu-linux.el
@@ -6,5 +6,5 @@
 ;  (color-theme-glow)
   (set-face-attribute 'default nil :font "DejaVu Sans Mono-10")
   (set-frame-height (selected-frame) 44)
-  (set-frame-width (selected-frame) 120))
+  (set-frame-width (selected-frame) 90))
 
