52496c31b2ff7711f877d73ddd84fabd6fcf47f4
Adam Collins <adam@m104.us>
2013-04-23 08:48:40 -0700

    Points of interest closing delimiter added

--- user-modes.el
+++ user-modes.el
@@ -1,7 +1,7 @@
 ;; highlight watch words (e.g. TODO, FIXME, XXX)
 (defun add-watchword-highlights ()
   (font-lock-add-keywords
-   nil '(("\\<\\(FIX\\(ME\\)?\\|TODO\\|HACK\\|XXX\\|NOTE\\|\\(DE\\)?BUG\\)"
+   nil '(("\\<\\(FIX\\(ME\\)?\\|TODO\\|HACK\\|XXX\\|NOTE\\|\\(DE\\)?BUG\\)\\>"
           1 highlight t))))
 
 (add-hook 'prog-mode-hook 'add-watchword-highlights)
